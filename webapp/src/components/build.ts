188f4520b3454601875523511ec0428d0c4ac97b
Michael Racette Olsén <micke.olsen@hotmail.com>
2023-01-05 22:50:54 +0100

    Place building functionality draft

---
+++ webapp/src/components/build.ts
@@ -0,0 +1,50 @@
+import { Component } from '.'
+import Connection from '../server/connection'
+import { GetPacket } from '../server/packet-get'
+import Game from './game'
+
+const ITEMS = ['TURRET']
+export default class Build extends Component {
+    buildContainer: HTMLDivElement
+    gridContainer: HTMLDivElement
+
+    constructor(private conn: Connection, private game: Game) {
+        super([])
+        this.buildContainer = document.getElementById(
+            'build-container'
+        ) as HTMLDivElement
+        this.gridContainer = document.getElementById(
+            'grid-container'
+        ) as HTMLDivElement
+
+        const elements = ITEMS.map((item) => {
+            const element = document.createElement('div')
+            element.textContent = item
+            element.onclick = () => {
+                this.onSelectItem(item)
+            }
+            return element
+        })
+
+        this.gridContainer.append(...elements)
+    }
+
+    onSelectItem(item: string) {
+        const ix = Math.floor(this.game.pos.x)
+        const iy = Math.floor(this.game.pos.y)
+
+        console.log(`Placing ${item} at (${ix},${iy})`)
+
+        this.conn.send({ type: 'build', item, ix, iy })
+    }
+
+    _focus() {}
+    _unfocus() {}
+    _show() {
+        this.buildContainer.style.display = 'flex'
+    }
+    _hide() {
+        this.buildContainer.style.display = 'none'
+    }
+    receive(pkg: GetPacket) {}
+}
--- webapp/src/server/packet-send.ts
+++ webapp/src/server/packet-send.ts
@@ -1,3 +1,5 @@
+import { Vec } from '../utils/math'
+
 export interface SendPacketJoin {
     type: 'join'
     username: string
@@ -17,9 +19,16 @@ export interface SendPacketPing {
     type: 'ping'
     timestamp: number
 }
+export interface SendPacketBuild {
+    type: 'build'
+    item: string
+    ix: number
+    iy: number
+}
 
 export type SendPacket =
     | SendPacketJoin
     | SendPacketMove
     | SendPacketChat
     | SendPacketPing
+    | SendPacketBuild
