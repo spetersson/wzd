671cd13c9c7d625485db695e5e0ab1b6e5b7ff9c
opkna <oscar.persson1993@gmail.com>
2023-01-07 23:24:11 +0100

    webapp: cleaned up tile calculation

--- webapp/src/utils/map/map.ts
+++ webapp/src/utils/map/map.ts
@@ -82,59 +82,85 @@ export async function getWorldMap() {
                         let tile3 = SpriteType.SUBTILE_WATER
                         let tile4 = SpriteType.SUBTILE_WATER
 
-                        // TILE 1:
-                        if (y - 1 > 0 && x - 1 > 0 && landMap[y - 1][x] && landMap[y][x - 1]) {
+                        const topLeft = y > 0 && x > 0 && landMap[y - 1][x - 1]
+                        const topMid = y > 0 && landMap[y - 1][x]
+                        const topRight = y > 0 && x + 1 < width && landMap[y - 1][x + 1]
+                        const leftMid = x > 0 && landMap[y][x - 1]
+                        const rightMid = x + 1 < width && landMap[y][x + 1]
+                        const botLeft = y + 1 < height && x > 0 && landMap[y + 1][x - 1]
+                        const botMid = y + 1 < height && landMap[y + 1][x]
+                        const botRight = y + 1 < height && x + 1 < width && landMap[y + 1][x + 1]
+
+                        // TILE 1 (Top Left):
+                        if (leftMid && topMid) {
                             tile1 = SpriteType.SUBTILE_CORNER_FRONT_L
-                        } else if (y - 1 > 0 && landMap[y - 1][x]) {
+                        } else if (topMid) {
                             tile1 = SpriteType.SUBTILE_FRONT
-                        } else if (x - 1 > 0 && landMap[y][x - 1]) {
+                        } else if (leftMid && topLeft) {
                             tile1 = SpriteType.SUBTILE_SIDE_L
-                        } else if (y - 1 > 0 && x - 1 > 0 && landMap[y - 1][x - 1]) {
+                        } else if (leftMid) {
+                            tile1 = SpriteType.SUBTILE_SIDE_BACK_L
+                        } else if (topLeft) {
                             tile1 = SpriteType.SUBTILE_POINT_FRONT_L
                         }
 
-                        // TILE 2:
-                        if (y - 1 > 0 && landMap[y - 1][x] && landMap[y][x + 1]) {
+                        // TILE 2 (Top Right):
+                        if (rightMid && topMid) {
                             tile2 = SpriteType.SUBTILE_CORNER_FRONT_R
-                        } else if (y - 1 > 0 && landMap[y - 1][x]) {
+                        } else if (topMid) {
                             tile2 = SpriteType.SUBTILE_FRONT
-                        } else if (x - 1 > 0 && landMap[y][x + 1]) {
+                        } else if (rightMid && topRight) {
                             tile2 = SpriteType.SUBTILE_SIDE_R
-                        } else if (y - 1 > 0 && landMap[y - 1][x + 1]) {
+                        } else if (rightMid) {
+                            tile2 = SpriteType.SUBTILE_SIDE_BACK_R
+                        } else if (topRight) {
                             tile2 = SpriteType.SUBTILE_POINT_FRONT_R
                         }
 
-                        // TILE 3:
-                        if (y + 1 < height && x - 1 > 0 && landMap[y + 1][x] && landMap[y][x - 1]) {
+                        // TILE 3 (Bot Left):
+                        if (botMid && leftMid) {
                             tile3 = SpriteType.SUBTILE_CORNER_BACK_L
-                        } else if (y + 1 < height && landMap[y + 1][x]) {
+                        } else if (botMid) {
                             tile3 = SpriteType.SUBTILE_BACK
-                        } else if (x - 1 > 0 && landMap[y][x - 1]) {
+                        } else if (leftMid) {
                             tile3 = SpriteType.SUBTILE_SIDE_L
-                        } else if (y + 1 < height && x > 0 && landMap[y + 1][x - 1]) {
+                        } else if (botLeft) {
                             tile3 = SpriteType.SUBTILE_POINT_BACK_L
                         }
 
-                        // TILE 4:
-                        if (y + 1 < height && x + 1 < width && landMap[y + 1][x] && landMap[y][x + 1]) {
+                        // TILE 4 (Bot Right):
+                        if (botMid && rightMid) {
                             tile4 = SpriteType.SUBTILE_CORNER_BACK_R
-                        } else if (y + 1 < height && landMap[y + 1][x]) {
+                        } else if (botMid) {
                             tile4 = SpriteType.SUBTILE_BACK
-                        } else if (x + 1 < width && landMap[y][x + 1]) {
+                        } else if (rightMid) {
                             tile4 = SpriteType.SUBTILE_SIDE_R
-                        } else if (y + 1 < height && x + 1 < width && landMap[y + 1][x + 1]) {
+                        } else if (botRight) {
                             tile4 = SpriteType.SUBTILE_POINT_BACK_R
                         }
 
-                        row[x] = {
-                            walkable: false,
-                            building: null,
-                            subtileSprites: [
-                                tileSprites[tile1],
-                                tileSprites[tile2],
-                                tileSprites[tile3],
-                                tileSprites[tile4],
-                            ],
+                        if (
+                            tile1 === SpriteType.SUBTILE_WATER &&
+                            tile2 === SpriteType.SUBTILE_WATER &&
+                            tile3 === SpriteType.SUBTILE_WATER &&
+                            tile4 === SpriteType.SUBTILE_WATER
+                        ) {
+                            row[x] = {
+                                walkable: false,
+                                building: null,
+                                tileSprite: tileSprites[SpriteType.TILE_WATER],
+                            }
+                        } else {
+                            row[x] = {
+                                walkable: false,
+                                building: null,
+                                subtileSprites: [
+                                    tileSprites[tile1],
+                                    tileSprites[tile2],
+                                    tileSprites[tile3],
+                                    tileSprites[tile4],
+                                ],
+                            }
                         }
                     }
                 }
